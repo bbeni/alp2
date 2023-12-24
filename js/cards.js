f7f0de1ff166306ab76329f338b21ad7f31f2884
Benjamin Froelich <bbenif@gmail.com>
2023-12-24 04:06:06 +0100

    test webhosting

---
+++ js/cards.js
@@ -0,0 +1,142 @@
+
+// teaser
+if (localStorage.length == 0) {
+    addCardToStorage(0, "Finish thesis", "Go and finish it!");
+    addCardToStorage(1, "Second TODO", "wash dishes");
+    addCardToStorage(2, "Collect leaves", "fun but not so important..");
+}
+
+function genCardsFromStorage() {
+    var keys = [];
+    for (var i = 0; i < localStorage.length; i++) {
+        key = localStorage.key(i)
+        keys.push(key);
+    }
+
+    var sortedKeys = keys.sort();
+    sortedKeys.forEach( (key) => {
+        value = JSON.parse(localStorage.getItem(key));
+            addCardAt(key, value[0], value[1])
+    });
+}
+
+function addCardToStorage(index, title, descr) {
+    localStorage.setItem(String(index), JSON.stringify([title, descr]));
+}
+
+
+function updateCardsInStorage() {
+    localStorage.clear();
+    const todoCards = document.querySelectorAll(".todo-card");
+    for (var i = 0; i < todoCards.length; i++) {
+        var title = todoCards[i].querySelector(".title");
+        var descr = todoCards[i].querySelector(".description");
+
+        addCardToStorage(i, title.innerHTML, descr.innerHTML);
+    }
+}
+
+
+// event listener for inserting the card into the table while dragging
+const changeCardTable = (e) => {
+    e.preventDefault();
+    const draggingCard = cardTable.querySelector(".dragging")
+    const others = [...cardTable.querySelectorAll(".todo-card:not(.dragging)")];
+    
+    let next = others.find(card => {
+        return e.clientY + window.scrollY <= card.offsetTop + card.offsetHeight /2;
+    })
+    cardTable.insertBefore(draggingCard, next);
+
+}
+
+
+cardTable.addEventListener("dragover", changeCardTable)
+
+
+
+const toTrashMoved = (e) => {
+    e.preventDefault();
+}
+
+const deletedCard = (e) => {
+    e.preventDefault();
+    const draggingCard = cardTable.querySelector(".dragging");
+    if (draggingCard) {
+        draggingCard.remove();
+    }
+
+    
+}
+
+trashArea = document.getElementById("trashArea");
+trashArea.addEventListener("dragover", toTrashMoved)
+trashArea.addEventListener("drop", deletedCard)
+
+
+function addCardAt(index, title, descr){
+    const content = `
+        <li class="todo-card" draggable="true">
+            <div class="todo-content">
+                <span class="title">${title}</span>
+                <span class="description">${descr}</span>
+            </div>
+            <i class="fa-solid fa-grip-lines"></i>
+        </li>
+    `;
+
+    cardTable.insertAdjacentHTML('beforeend', content);
+
+    // Rebind drag events to all todo cards
+    bindDragEvents();
+    addCardToStorage(index, title, descr);
+}
+
+// add a new Card item
+function addCard() {
+    const title = document.querySelector("#newTitle").value;
+    const descr = document.querySelector("#newDescr").value;
+
+    if (title.trim() === "") {
+        return;
+    }
+
+    addCardAt(localStorage.length, title, descr);
+
+    var popup = document.getElementById("addPage");
+    popup.classList.toggle("show");
+
+    document.querySelector("#newTitle").value = "";
+    document.querySelector("#newDescr").value = "";
+
+}
+
+// Function to bind drag events to all todo cards
+function bindDragEvents() {
+    const todoCards = document.querySelectorAll(".todo-card");
+
+    todoCards.forEach(card => {
+        // Remove existing event listeners to avoid duplicates
+        card.removeEventListener("dragstart", dragStart);
+        card.removeEventListener("dragend", dragEnd);
+
+        // Add new event listeners
+        card.addEventListener("dragstart", dragStart);
+        card.addEventListener("dragend", dragEnd);
+    });
+}
+
+// Event listener functions
+function dragStart() {
+    setTimeout(() => this.classList.add("dragging"), 0);
+}
+
+function dragEnd() {
+    this.classList.remove("dragging");
+    updateCardsInStorage();
+}
+
+genCardsFromStorage();
+bindDragEvents();
+
+
